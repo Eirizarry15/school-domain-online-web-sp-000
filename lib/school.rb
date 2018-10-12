f6556cc5425ea954f4ab05f5f4b5cf1d88b5fd8c
Edgar Irizarry <eirizarry15@yahoo.com>
2018-10-12 18:48:50 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,5 +1,33 @@
 # code here!
 
-class school()
+class school
+ 
+ def initalize(school_name)
+   @school_name = school_name
+   @roster = {} 
+end 
+
+def roster 
+  @roster 
+end 
+
+def add_student(name, grade)
+  @student_name = name 
+  @grade = grade 
+  if@roster.include?(grade) == false 
+    @roster[grade] = []
+  end 
+  @roster[grade] << name 
+end 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
   
 end 
